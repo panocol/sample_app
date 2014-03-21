68a053657bbf2272777b8f24dfe05ec682606316
pnc <patrickncollins@gmail.com>
2014-03-21 09:31:31 -0400

    commit

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,12 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title("Ruby on Rails Tutorial Sample App | Sign up") }
   end
+
+  describe "profile page" do
+    # Replace with code to make a user variable
+    before { visit user_path(1) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
